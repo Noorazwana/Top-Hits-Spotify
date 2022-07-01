f890d83a3adc004c408383050ef8a0d00779e95b
Noorazwana <108342940+Noorazwana@users.noreply.github.com>
2022-07-01 17:12:06 +0800

    Update TopHits_Classification.py

--- TopHits_Classification.py
+++ TopHits_Classification.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gs
-import seaborn as sns
+
 sns.set()
 import warnings
 warnings.filterwarnings('ignore')
